e35745fa4ae748c60aea3f2f2d286cb4dc696f17
EeethB <ethanbrockmann@gmail.com>
2021-06-29 22:37:38 -0500

    updated index to use image rather than iframe
    also updated some titles

--- 01_hello_custom/app.R
+++ 01_hello_custom/app.R
@@ -4,7 +4,7 @@ library(shiny)
 ui <- fluidPage(
 
   # App title ----
-  titlePanel("Hello Shiny!"),
+  titlePanel("Old Faithful eruption times"),
 
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
